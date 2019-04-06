64bbe3be41ecbfc6eb273aa2d4038a9fd2224349
Shruti Varghese <vtshruti@gmail.com>
2019-04-06 23:16:34 +0530

    Add files via upload
    Added all files except images and sliders.

---
+++ js/jq.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+	    interval: 10000
+	})
+});
+$(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 500) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
